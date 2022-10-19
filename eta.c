3fc52fb9f9a2bce95bc9ae3dc3cc8cb2b64fe963
Riccardo Rodini <113691534+Riccardo-Rodini@users.noreply.github.com>
2022-10-19 14:01:42 +0200

    Add files via upload

---
+++ eta.c
@@ -0,0 +1,19 @@
+/*
+file:
+autore: 
+classe: 
+data:
+*/
+#include<stdio.h>
+int main()
+{
+	int eta;
+	printf("inserisci la tua eta");
+	scanf("%d",&eta);
+	if(eta>=18)
+	{printf("e' maggiorenne");
+	}
+	else
+	{printf("non e maggiorenne");
+	}
+}
